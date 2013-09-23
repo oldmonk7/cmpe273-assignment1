1f58ad1352eabadb4fc7f1cab00be5fc0b06206d
Shashank <shashankgarg7@gmail.com>
2013-09-22 17:26:57 -0700

    added domain

---
+++ src/main/java/edu/sjsu/cmpe/library/domain/Author.java
@@ -0,0 +1,24 @@
+package edu.sjsu.cmpe.library.domain;
+
+public class Author {
+	
+	private long id;
+	private String name;
+	
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+
+}
--- src/main/java/edu/sjsu/cmpe/library/domain/Book.java
+++ src/main/java/edu/sjsu/cmpe/library/domain/Book.java
@@ -1,12 +1,21 @@
 package edu.sjsu.cmpe.library.domain;
-
+import java.util.Date;
 
 public class Book {
     private long isbn;
     private String title;
-
-    // add more fields here
-
+    private Date pubDate;
+    private String language;
+    private int numPages;
+    private String status = "available";
+    /*add more fields here
+    ISBN (Key) # You will generate this key.
+   Title (Required field)
+ Publication Date (Required field)
+Language (Optional field)
+Number of Pages (Optional field)
+Status {available, checked-out, in-queue, or lost} # default to ‘available’
+     */
     /**
      * @return the isbn
      */
@@ -36,4 +45,47 @@ public class Book {
     public void setTitle(String title) {
 	this.title = title;
     }
+    
+    public Date getPublicationDate() {
+    	return pubDate;
+        }
+    
+    public void setPublicationDate(Date date) {
+    	this.pubDate = date;
+        }
+    
+    public String getLanguage() {
+    	return language;
+        }
+
+        /**
+         * @param title
+         *            the title to set
+         */
+        public void setLanguage(String language) {
+    	this.language = language;
+        }
+        public int getNumPages() {
+        	return numPages;
+            }
+
+            /**
+             * @param isbn
+             *            the isbn to set
+             */
+            public void setNumPages(int numPages) {
+        	this.numPages = numPages;
+            }
+            
+            public String getStatus() {
+            	return status;
+                }
+
+                /**
+                 * @param title
+                 *            the title to set
+                 */
+                public void setStatus(String status) {
+            	this.status = status;
+                }
 }
